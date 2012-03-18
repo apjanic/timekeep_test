c25216cb39bf21f1b2c8bd8e7c74c236bccb9e18
Ademir Pjanic <apjanic@capeannenterprises.com>
2012-03-18 21:38:23 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimekeepTest::Application.config.secret_token = 'c61e8ae9e870776628d572705139ccc911c9f3f48f4ee4b2c7724dc8cbb86941fda2ef0a10b1cc43afb8f987c1795c50f5384a574e4e474b50390f52623676a3'
